8263060f04cebf025f09b5f58078d09937228975
GabrielFaria23 <gabrielfariapta01@gmail.com>
2021-01-25 18:15:39 -0300

    feat: created header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AboutMeComponent } from './about-me/about-me.component';
 import { GitProjectsComponent } from './git-projects/git-projects.component';
 import { ContactInfoComponent } from './contact-info/contact-info.component';
 import { KnowledgeInfoComponent } from './knowledge-info/knowledge-info.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +15,9 @@ import { KnowledgeInfoComponent } from './knowledge-info/knowledge-info.componen
     AboutMeComponent,
     GitProjectsComponent,
     ContactInfoComponent,
-    KnowledgeInfoComponent
+    KnowledgeInfoComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
